ecb7c732648dbe91f7c58c071257d19979ebdaf1
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-05-09 00:28:32 +0200

    Add some extra and record scripts.

---
+++ Awecode/Record/Prepare.php
@@ -0,0 +1,33 @@
+<?php
+
+require '/usr/local/lib/hoa/Core/Core.php';
+
+from('Hoa')
+-> import('Console.Processus')
+-> import('Console.Window');
+
+$windowSize = Hoa\Console\Window::getSize();
+$screenSize = array('x' => 0, 'y' => 0);
+
+if(null !== $osascript = Hoa\Console\Processus::locate('osascript')) {
+
+    $handle = Hoa\Console\Processus::execute(
+        $osascript .
+        ' -e \'tell application "Finder" to get bounds of window of desktop\'',
+        false
+    );
+
+    list(,, $screenSize['x'], $screenSize['y']) = preg_split('#\s*,\s*#', $handle);
+}
+elseif(null !== $xwininfo = Hoa\Console\Processus::locate('xwininfo')) {
+
+    preg_match(
+        '#\-geometry (\d+)x(\d+)#m',
+        Hoa\Console\Processus::execute($xwininfo . ' -root'),
+        $matches
+    );
+    list(, $screenSize['x'], $screenSize['y']) = $matches;
+}
+
+Hoa\Console\Window::setSize(89, 26);
+Hoa\Console\Window::moveTo(212, 174);
