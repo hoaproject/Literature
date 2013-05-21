1e36ffc87fcdfbe3699abad919ba3ea679f9478a
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-05-21 09:33:02 +0200

    Add a movie to gif converter.

---
+++ Awecode/Bin/ToGif.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+ffmpeg=`which ffmpeg`
+
+if [[ "x" = "x$ffmpeg" ]]; then
+    echo "ffmpeg binary is not found."
+    exit 1
+fi
+
+convert=`which convert`
+
+if [[ "x" = "x$convert" ]]; then
+    echo "convert binary is not found."
+    exit 1
+fi
+
+workspace=/tmp/awecode.hoa/$RANDOM/
+input=""
+output=""
+
+mkdir -p $workspace
+
+function usage() {
+
+    echo >&2 "Usage:"
+    echo >&2 "    $0 -i=<input> -o=<output>"
+}
+
+while getopts i:o:h opt; do
+    case $opt in
+        i)   input=$OPTARG;;
+        o)   output=$OPTARG;;
+        h)   usage; exit 2;;
+        [?]) usage; exit 2;;
+    esac
+done
+
+if [[ -z "$input" ]]; then
+    echo '-i is empty.'
+    usage
+    exit 3
+fi
+
+if [[ -z "$output" ]]; then
+    echo '-o is empty.'
+    usage
+    exit 3
+fi
+
+$ffmpeg -i $input -r 10 $workspace/temp%03d.gif
+$convert -delay 5 -loop 0 $workspace/temp*.gif $workspace/output.gif
+$convert -layers Optimize $workspace/output.gif $output
+
+echo "[done]"
+echo "    workspace: $workspace"
+echo "    output file: $output"
