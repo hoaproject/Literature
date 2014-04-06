6f2990e69c00e41d9c546b6ce9e3dd5bc1706c99
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-04-06 19:35:48 +0200

    Reduce file size with higher quality.
    `ffmpeg` does a bad job while converting to GIF, so we convert to PNG.
    We reduce file size by disabling dither and fuzzing 1% of the colors. Also,
    the `-c` option is added to control the number of colors in the output
    palette. Finally, because `gifsicle` has a better LWZ compression
    optimization than `convert`, we finalize the size reducing with
    `gifsicle`.
    
    Also, we have added the `-d` option to control the delay between to
    frame (we can find the balance between frame rate and delay to still
    reduce the file size).
    
    Note: on my example, 254Kb to 119Kb with an equivalent quality to the
    video (before, I got white dots everywhere)!

--- Awecode/Bin/ToGif.sh
+++ Awecode/Bin/ToGif.sh
@@ -14,10 +14,19 @@ if [[ "x" = "x$convert" ]]; then
     exit 1
 fi
 
+gifsicle=`which gifsicle`
+
+if [[ "x" = "x$gifsicle" ]]; then
+    echo "gifsicle binary is not found."
+    exit 1
+fi
+
 workspace=/tmp/awecode.hoa/$RANDOM/
 input=""
 output=""
 frame_rate=10
+delay=5
+colors=16
 
 mkdir -p $workspace
 
@@ -25,13 +34,23 @@ function usage() {
 
     echo >&2 "Usage:"
     echo >&2 "    $0 -i=<input> -o=<output>"
+    echo >&2 "Options:"
+    echo >&2 "    (input)"
+    echo >&2 "    -i  input file"
+    echo >&2 "    -r  frame rate (default: $frame_rate)"
+    echo >&2 "    (output)"
+    echo >&2 "    -o  output file"
+    echo >&2 "    -d  delay between each frame (default: $delay)"
+    echo >&2 "    -c  number of colors (default: $colors)"
 }
 
-while getopts i:o:r:h opt; do
+while getopts i:o:r:d:c:h opt; do
     case $opt in
         i)   input=$OPTARG;;
         o)   output=$OPTARG;;
         r)   frame_rate=$OPTARG;;
+        d)   delay=$OPTARG;;
+        c)   colors=$OPTARG;;
         h)   usage; exit 2;;
         [?]) usage; exit 2;;
     esac
@@ -49,9 +68,10 @@ if [[ -z "$output" ]]; then
     exit 3
 fi
 
-$ffmpeg -i $input -r $frame_rate $workspace/temp%03d.gif
-$convert -delay 5 -loop 0 $workspace/temp*.gif $workspace/output.gif
-$convert -layers Optimize $workspace/output.gif $output
+$ffmpeg -i $input -r $frame_rate $workspace/temp%03d.png
+$convert -fuzz 1% -delay $delay -loop 0 $workspace/temp*.png \
+         +dither -layers Optimize -colors $colors $output
+$gifsicle -O9 $output -o $output
 
 echo "[done]"
 echo "    workspace: $workspace"
